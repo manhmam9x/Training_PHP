f224d2f21e7f24476aff15764873ddde68e71e70
Hoang Manh <manhmam9x@gmail.com>
2022-11-04 15:56:48 +0700

    PHP Basic

---
+++ PHP_Basic/bai1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$n = '';
+if (isset ( $_POST ['n'] )) {
+    $n = $_POST ['n'];
+    function checkValidString($n){
+        $array = explode(' ', $n);
+        if (in_array('book', $array)){
+            echo 'chuỗi hợp lệ';
+        }elseif (in_array('restaurant', $array)){
+            echo 'chuỗi hợp lệ';
+        }else{
+            echo 'chuỗi không hợp lệ ';
+        }
+    }
+    echo checkValidString($n);
+}
+?>
+<form action="#" method="post">
+ <table>
+  <tr>
+   <td>Nhập S:</td>
+   <td><input type="text" name="n" value="<?=$n?>" /></td>
+  </tr>
+  <tr>
+   <td></td>
+   <td><input type="submit" value="Phân tích"></td>
+  </tr>
+ </table>
+</form>
